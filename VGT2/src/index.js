61d9bba5165e4a6f0dcc421103f9d779d1d6039f
WilliamW5 <WilliamWiemann@outlook.com>
2022-02-07 22:30:03 -0500

    initial commit

---
+++ VGT2/src/index.js
@@ -0,0 +1,25 @@
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+ctx.clearRect(0, 0, 800, 600)
+
+let game = new Game(GAME_WIDTH, GAME_HEIGHT);
+
+let lastTime = 0;
+function gameLoop(timeStamp) {
+    let deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+    game.update(deltaTime);
+    game.draw(ctx);
+
+    // gameloop and passes timestamp
+    requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
